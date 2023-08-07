c162db18a5f99c6723ab81a3a5e571b89ba18a6a
Arad <109275960+Aradpython@users.noreply.github.com>
2023-08-07 10:24:47 +0330

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -165,14 +165,6 @@ USE_TZ = True
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'rezakhani.arad@gmail.com'
-# EMAIL_HOST_PASSWORD = ''
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
